7f9bdeba5622e89aefc57c20a339af980cf0af62
Jim <jim@jam.sessionsnet.org>
2023-03-15 22:13:09 -0700

    sender.py, has definitions of each char, and time spacings

---
+++ sender.py
@@ -0,0 +1,54 @@
+m = {
+      'A': '.-',
+      'B': '-...',
+      'C': '-.-.',
+      'D': '-..',
+      'E': '.',
+      'F': '..-.',
+      'G': '--.',
+      'H': '....',
+      'I': '..',
+      'J': '.---',
+      'K': '-.-',
+      'L': '.-..',
+      'M': '--',
+      'N': '-.',
+      'O': '---',
+      'P': '.--.',
+      'Q': '--.-',
+      'R': '.-.',
+      'S': '...',
+      'T': '-',
+      'U': '..-',
+      'V': '...-',
+      'W': '.--',
+      'X': '-..-',
+      'Y': '-.--',
+      'Z': '--..',
+      '1': '.----',
+      '2': '..---',
+      '3': '...--',
+      '4': '....-',
+      '5': '.....',
+      '6': '-....',
+      '7': '--...',
+      '8': '---..',
+      '9': '----.',
+      '0': '-----',
+    }
+
+# how much time a "dit" is, in time interval terms
+ditTime = 40 # decide what unit of time
+
+# how many ditTimes a "dah" is
+dahDits = 3
+
+# how many ditTimes between "elements" of an individual char
+elemDits = 1
+
+# how many ditTimes between chars
+charSpacingDits = 3
+
+# how many ditTimes between words
+wordSpacingDits = 7
+
